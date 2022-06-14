0305b451464da09c1a5631c73da4590a1548cdbf
Roee Shapira <roee.shapira@tensorleap.ai>
2022-06-14 21:55:56 +0300

    Add needed crates

--- crates/vm/src/vm.rs
+++ crates/vm/src/vm.rs
@@ -3,20 +3,29 @@ use super::instr::Instr;
 pub struct VM {
 	pub ip: usize,
 	pub stack: Vec<isize>,
+
+	pub memory_pointer: usize,
+	pub memory: Vec<isize>,
 }
 
 impl Default for VM {
 	fn default() -> Self {
 		Self {
 			ip: 0,
-			stack: Vec::with_capacity(30_000),
+			stack: Vec::new(),
+			memory_pointer: 0,
+			memory: Vec::with_capacity(30_000),
 		}
 	}
 }
 
 impl VM {
-	pub fn handle_instr(&self, instr: &Instr) {
-		todo!("Handle {:?} instruction", instr)
+	pub fn handle_instr(&mut self, instr: &Instr) {
+		match instr {
+			Instr::MoveRight(n) => self.memory_pointer += n,
+			Instr::MoveLeft(n) => self.memory_pointer -= n,
+			other => todo!("Handle instr {:?}", other),
+		}
 	}
 
 	pub fn run(&mut self, program: &[Instr]) -> Option<isize> {
