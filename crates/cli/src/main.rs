f33628a3a302b5f96cc9c97a2ca39dcfebfd29e5
Roee Shapira <roee.shapira@tensorleap.ai>
2022-06-16 17:15:38 +0300

    Add cli init

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,3 +1,22 @@
+use clap::{self, Parser};
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Argv {
+	/// The program's input
+	#[clap(value_parser)]
+	input: Option<String>,
+	/// The program's source code file
+	#[clap(short, long, value_parser, value_name = "FILE")]
+	file: Option<PathBuf>,
+	/// The program's source code as a string
+	#[clap(short, long, value_parser, value_name = "EVAL")]
+	eval: Option<String>,
+}
+
 fn main() {
-	println!("Hello, world!");
+	let argv = Argv::parse();
+
+	println!("input: {:?}", argv.input);
 }
