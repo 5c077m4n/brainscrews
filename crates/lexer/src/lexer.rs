c9970ce3b32874ce7be747c31a0affcdf4407de6
Roee Shapira <roee.shapira@tensorleap.ai>
2022-06-22 16:12:41 +0300

    Add Token from trait

--- crates/lexer/src/lexer.rs
+++ crates/lexer/src/lexer.rs
@@ -1,16 +1,6 @@
 use super::tokens::Token;
 
 pub fn lex(input: &'static str) -> Box<impl Iterator<Item = Token>> {
-	let token_iter = input.as_bytes().iter().map(|c| match c {
-		b'>' => Token::PointerRight,
-		b'<' => Token::PointerLeft,
-		b'+' => Token::CellInc,
-		b'-' => Token::CellDec,
-		b',' => Token::Insert,
-		b'.' => Token::Print,
-		b'[' => Token::LoopStart,
-		b']' => Token::LoopEnd,
-		other => Token::Comment(*other),
-	});
+	let token_iter = input.as_bytes().iter().map(Token::from);
 	Box::new(token_iter)
 }
--- crates/lexer/src/tokens.rs
+++ crates/lexer/src/tokens.rs
@@ -19,3 +19,19 @@ pub enum Token {
 	/// Any other character is considered a comment
 	Comment(u8),
 }
+
+impl From<&u8> for Token {
+	fn from(c: &u8) -> Self {
+		match *c {
+			b'>' => Token::PointerRight,
+			b'<' => Token::PointerLeft,
+			b'+' => Token::CellInc,
+			b'-' => Token::CellDec,
+			b',' => Token::Insert,
+			b'.' => Token::Print,
+			b'[' => Token::LoopStart,
+			b']' => Token::LoopEnd,
+			other => Token::Comment(other),
+		}
+	}
+}
